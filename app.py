093466448858b866fd0344170c9b61cc7fcef191
Fer123a <fernando.laube@gmail.com>
2021-04-02 17:08:00 -0300

    Add temp files initial test

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from helpers import calculate_percentages, order_dict
 from chess_dot_com_helpers import get_chess_dot_com_moves_history
 from lichess_helpers import get_lichess_moves_history
 import json
+import os
+import time
 
 # Configure application
 app = Flask(__name__)
@@ -19,6 +21,7 @@ Session(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    start_whole_page = time.time()
     if request.method == "GET":
         session.clear()
         # Render home page
@@ -39,12 +42,30 @@ def index():
         if not username:
             return render_template("index.html", error="Username not found!")
 
+        file_path = app.config["SESSION_FILE_DIR"] + "/test_file"
+
         if platform == "chess_dot_com":
-            moves_history = get_chess_dot_com_moves_history(username=username,
-                                                            color=color,
-                                                            rating=rating,
-                                                            time_class=time_class,
-                                                            time_control=time_control)
+            
+            if os.path.isfile(file_path) and os.path.getsize(file_path) > 0:
+                start = time.time()
+                with open(file_path, 'r') as f:
+                    moves_history = json.load(f)
+                end = time.time()
+                print(f"Time elapsed to load moves_history from file: {end-start}")
+
+            else:
+                moves_history = get_chess_dot_com_moves_history(username=username,
+                                                                color=color,
+                                                                rating=rating,
+                                                                time_class=time_class,
+                                                                time_control=time_control)
+                start = time.time()
+                with open(file_path, 'w') as f:
+                    f.write(json.dumps(moves_history) + "\n")
+
+                    #json.dump(moves_history, f)
+                end = time.time()
+                print(f"Time elapsed to save moves_history to file: {end-start}")
 
         if platform == "lichess":
             moves_history = get_lichess_moves_history(username=username,
@@ -57,6 +78,9 @@ def index():
             calculate_percentages(moves_history)
 
             moves_history = order_dict(moves_history)
+
+            end_whole_page = time.time()
+            print(f"Time elapsed before rendering the explorer page: {end_whole_page-start_whole_page}")
 
             if not moves_history["next_moves"]:
                 return render_template("index.html", error="No game matching selected options has been found!")
