248a2985573a7fdb764757e545f99da4d6f50f63
Fer123a <fernando.laube@gmail.com>
2021-04-24 16:27:10 -0300

    Refactor all modules

--- chess_dot_com_helpers.py
+++ chess_dot_com_helpers.py
@@ -1,9 +1,20 @@
-import requests
+"""
+Implement the functions responsible for handling the Chess.com API requests
+and building the moves history obtained from those requests.
+"""
 import re
+import requests
+from helpers import increment_move_in_moves_dict, create_move_in_moves_dict
 
 
-def get_chess_dot_com_moves_history(username, color, rating,
-                                    time_class, time_control):
+# Define regex pattern used multiple times
+RESULT_REGEX_PATTERN = r"\[Result \"(?P<result>[1, /, 2, 0]+-[1, /, 2, 0]+)\"]"
+WHITE_MOVE_REGEX_PATTERN = r"\. (?P<move>[a-z, A-Z, 1-9, +, #, =, -]+) {"
+BLACK_MOVE_REGEX_PATTERN = r"\.\.\. (?P<move>[a-z, A-Z, 1-9, +, #, =, -]+) {"
+
+
+def get_chess_dot_com_moves_history(username: str, color: str, rating: int,
+                                    time_class: str, time_control: str):
     """
     Construct the moves history from chess.com archives
     """
@@ -13,52 +24,67 @@ def get_chess_dot_com_moves_history(username, color, rating,
     if archives_list is not None:
         moves_history = {"next_moves": {}}
         with requests.Session() as session:
+            games = []
             for url in archives_list:
                 response = session.get(url)
                 response = response.json()
 
-                games = response["games"]
+                # games = response["games"]
+                games.extend(response["games"])
 
-                for game in games:
+            filtered_games = (game for game in games
+                                if is_filter_satisfied(game,
+                                                       username,
+                                                       color,
+                                                       rating,
+                                                       time_class,
+                                                       time_control))
 
-                    # Don't consider chess variations
-                    # other than the classical one
-                    if game["rules"] == "chess":
+            for game in filtered_games:
+                # Parse each game from the archive
+                try:
+                    pgn = game["pgn"]
 
-                        # Apply filters
-                        # Filter by color
-                        if game[color]["username"] == username:
-
-                            # Filter by rating
-                            if rating:
-                                if int(game[color]["rating"]) < int(rating):
-                                    continue
-
-                            # Filter by time class
-                            if time_class:
-                                if game["time_class"] != time_class:
-                                    continue
-
-                            # Filter by time control
-                            if time_control:
-                                if game["time_control"] != time_control:
-                                    continue
-
-                            # Parse each game from the archive
-                            try:
-                                pgn = game["pgn"]
-
-                                parse_pgn(moves_history, pgn)
-                            except KeyError:
-                                pass
+                    parse_pgn(moves_history, pgn)
+                except KeyError:
+                    pass
         return moves_history
-    else:
-        return None
+
+    return None
+
+
+def is_filter_satisfied(game, username, color, rating, time_class, time_control):
+    """
+    Returns True if all filtering conditions are satisfied,
+    returns False otherwise
+    """
+    # Filter chess variations
+    if game["rules"] == "chess":
+        # Filter by color
+        if game[color]["username"] == username:
+            # Filter by rating
+            if rating:
+                if int(game[color]["rating"]) < rating:
+                    return False
+
+            # Filter by time class
+            if time_class:
+                if game["time_class"] != time_class:
+                    return False
+
+            # Filter by time control
+            if time_control:
+                if game["time_control"] != time_control:
+                    return False
+
+            return True
+
+    return False
 
 
 def get_monthly_archives(username):
     """
-    Make a request to chess.com API to obtain a list
+    Make a request to chess.com API to obtain a structure
     containing the list of monthly archives available for the player.
 
     The monthly archive is a url to which an API request must be made
@@ -88,8 +114,8 @@ def parse_pgn(moves_dict, pgn, current_move=1):
     white_to_move = True
 
     # Get result of the game to calculate the winning percentage later
-    m = re.search(r"\[Result \"(?P<result>[1, /, 2, 0]+-[1, /, 2, 0]+)\"]", pgn)
-    result = m.group("result")
+    result_search = re.search(RESULT_REGEX_PATTERN, pgn)
+    result = result_search.group("result")
 
     if result == "1-0":
         result = 1
@@ -116,34 +142,28 @@ def parse_pgn(moves_dict, pgn, current_move=1):
 
 
 def parse_move(moves_dict, pgn, current_move, white_to_move, result):
+    """
+    Find the move given by current_move in the pgn,
+    parse it and add it to the moves dictionary
+    """
 
     if white_to_move:
         # Find current move by white
-        m = re.search(str(current_move)+r"\. (?P<move>[a-z, A-Z, 1-9, +, #, =, -]+) {", pgn)
+        move_search = re.search(str(current_move) + WHITE_MOVE_REGEX_PATTERN, pgn)
     else:
         # Find current move by black
-        m = re.search(str(current_move)+r"\.\.\. (?P<move>[a-z, A-Z, 1-9, +, #, =, -]+) {", pgn)
+        move_search = re.search(str(current_move) + BLACK_MOVE_REGEX_PATTERN, pgn)
 
     # Check if a move was played in this position
-    if m is not None:
-        move = m.group("move")
+    if move_search is not None:
+        move = move_search.group("move")
 
         if move in moves_dict["next_moves"]:
-            moves_dict["next_moves"][move]["count"] += 1
-            if result == 1:
-                moves_dict["next_moves"][move]["white_wins"] += 1
-            elif result == 0:
-                moves_dict["next_moves"][move]["black_wins"] += 1
+            increment_move_in_moves_dict(moves_dict, move, result)
         else:
-            if result == 1:
-                moves_dict["next_moves"][move] = {"count": 1, "white_wins": 1, "black_wins": 0, "next_moves": {}}
-            elif result == 0:
-                moves_dict["next_moves"][move] = {"count": 1, "white_wins": 0, "black_wins": 1, "next_moves": {}}
-            else:
-                moves_dict["next_moves"][move] = {"count": 1, "white_wins": 0, "black_wins": 0, "next_moves": {}}
+            create_move_in_moves_dict(moves_dict, move, result)
 
         return moves_dict["next_moves"][move]
 
-    else:
-        # No move was played in this position - the game ended here
-        return None
+    # No move was played in this position - the game ended here
+    return None
